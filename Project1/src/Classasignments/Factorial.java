30cb445d603200f8979062191ce63b451c083757
SAchinkUmar <freshenergy@springtimesoftware.8et>
2022-06-23 23:27:07 +0530

    Single Loop Assignments are Done

--- Project1/src/Classasignments/Factorial.java
+++ Project1/src/Classasignments/Factorial.java
@@ -9,10 +9,10 @@ public class Factorial {
 		Scanner s=new Scanner(System.in);
 		int num=s.nextInt();
 		int fact=1;
-		for (int i=1;i<=num;i++) {
-			fact=fact*i;
+		for (int i=1;i<=num;i++) { //1,2,3,4,5,6
+			fact=fact*i;//  1*1=1,2*1=2,2*3=6,6*4=24,24*5=120
 		}
-		System.out.println("Factorial Of a Number is : " + num + fact);
+		System.out.println("Factorial Of a " + num + " Is : "+ fact);
 	}
 
 }
